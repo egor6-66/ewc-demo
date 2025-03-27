2ec65a3a0715ee6c8cd2f64602cc03024ec5c9c5
Raybul <egorraybul@gmail.com>
2025-03-27 14:28:08 +0300

    upd

--- modules/host/src/pages/workspace/incidents/index.tsx
+++ modules/host/src/pages/workspace/incidents/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouting, useStandalone } from '@packages/hooks';
 import { Modules } from '@packages/types';
-import { AnimatePresence, Button } from '@packages/ui';
+import { AnimatePresence, Button, GridLayout, IGridLayout } from '@packages/ui';
 
 import { CardPreview, CardsList, Map } from '@/widgets';
 
@@ -12,6 +12,12 @@ const IncidentsPage = () => {
 
     const mapStandalone = useStandalone({ currentModule: Modules.HOST, targetModule: Modules.MAP });
 
+    const widgets: IGridLayout.Items = [
+        { name: 'card_list', grid: { x: 1, y: 1, h: 11, w: 3 } },
+        { name: 'card_preview', grid: { x: 2, y: 1, h: 11, w: 1 } },
+        { name: 'map', grid: { x: 1, y: 2, h: 9, w: 4 } },
+    ];
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.header}>
@@ -21,17 +27,26 @@ const IncidentsPage = () => {
                 </div>
                 <div className={styles.block}>filters</div>
             </div>
-            <div className={styles.cardGroup}>
-                <div className={styles.cardList}>
-                    <CardsList />
-                </div>
-                <div className={styles.cardPreview}>
-                    <CardPreview />
-                </div>
+            <div className={styles.gridContainer}>
+                <GridLayout items={widgets} className={styles.grid}>
+                    {(item) => {
+                        switch (item.name) {
+                            case 'card_list':
+                                return <CardsList />;
+
+                            case 'card_preview':
+                                return <CardPreview />;
+
+                            case 'map':
+                                return (
+                                    <AnimatePresence visible={!mapStandalone.value} className={styles.map} animationVariant={'fullHeight'}>
+                                        <Map />
+                                    </AnimatePresence>
+                                );
+                        }
+                    }}
+                </GridLayout>
             </div>
-            <AnimatePresence visible={!mapStandalone.value} className={styles.map} animationVariant={'fullHeight'}>
-                <Map />
-            </AnimatePresence>
         </div>
     );
 };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,14 +1,16 @@
 import * as IDropdown from './dropdown/interfaces';
+import * as IGridLayout from './gridLayout/interfaces';
 import * as INavigation from './navigation/interfaces';
 import * as ITab from './tabs/interfaces';
 import AnimatePresence from './animatePresence';
 import Button from './button';
 import Checkbox from './checkbox';
 import Dropdown from './dropdown';
+import GridLayout from './gridLayout';
 import Icons from './icons';
 import Input from './input';
 import Navigation from './navigation';
 import Tabs from './tabs';
 
-export { AnimatePresence, Button, Checkbox, Dropdown, Icons, Input, Navigation, Tabs };
-export type { IDropdown, INavigation, ITab };
+export { AnimatePresence, Button, Checkbox, Dropdown, GridLayout, Icons, Input, Navigation, Tabs };
+export type { IDropdown, IGridLayout, INavigation, ITab };
